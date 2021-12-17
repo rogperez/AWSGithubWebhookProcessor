879425424e75a4b451efee419ed9eb3e943ba11b
Roger Perez <rperez@verimatrix.com>
2021-12-16 19:49:33 -0700

    Change name of stack

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,7 +4,7 @@ import * as cdk from "@aws-cdk/core";
 import { GithubWebhookProcessor } from "../lib/github-webhook-processor-stack";
 
 const app = new cdk.App();
-new GithubWebhookProcessor(app, "AWSGithubWebhookProcessor", {
+new GithubWebhookProcessor(app, "GithubWebhookProcessor", {
   supportedWebhooks: ["organization", "member"],
   tags: {
     Project: "GithubWebhooks",
