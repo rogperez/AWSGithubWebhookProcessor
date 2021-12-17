78cc78e507729619a18741bc94ad713f214f4b00
Roger Perez <rogperez52@gmail.com>
2021-12-16 20:46:04 -0700

    Add response to http request

--- infrastructure/lib/github-webhook-processor-stack.ts
+++ infrastructure/lib/github-webhook-processor-stack.ts
@@ -1,8 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as sqs from "@aws-cdk/aws-sqs";
 import * as iam from "@aws-cdk/aws-iam";
-import { Integration, IntegrationType, RestApi } from "@aws-cdk/aws-apigateway";
-import { Role, ServicePrincipal } from "@aws-cdk/aws-iam";
+import * as apigw from "@aws-cdk/aws-apigateway";
 import { Aws } from "@aws-cdk/core";
 
 export interface GithubWebhookProcessorProps extends cdk.StackProps {
@@ -22,21 +21,22 @@ export class GithubWebhookProcessor extends cdk.Stack {
      * Role
      */
     const role = new iam.Role(this, "QueueSendMessageRole", {
-      assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
     });
     queue.grantSendMessages(role);
 
     /**
      * API
      */
-    const apiGateway = new RestApi(this, "API");
+    const apiGateway = new apigw.RestApi(this, "GithubWebhook");
     apiGateway.root.addMethod(
       "POST",
-      new Integration({
-        type: IntegrationType.AWS,
+      new apigw.Integration({
+        type: apigw.IntegrationType.AWS,
         integrationHttpMethod: "POST",
         uri: `arn:${Aws.PARTITION}:apigateway:${Aws.REGION}:sqs:path/${Aws.ACCOUNT_ID}/${queue.queueName}`,
         options: {
+          passthroughBehavior: apigw.PassthroughBehavior.NEVER,
           credentialsRole: role,
           requestParameters: {
             "integration.request.header.Content-Type": "'application/json'",
@@ -44,8 +44,26 @@ export class GithubWebhookProcessor extends cdk.Stack {
           requestTemplates: {
             "application/json": "Action=SendMessage&MessageBody=$input.body",
           },
+          integrationResponses: [
+            {
+              statusCode: "200",
+              responseTemplates: {
+                "application/json": "success",
+              },
+            },
+          ],
         },
-      })
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: "200",
+            responseModels: {
+              "application/json": apigw.Model.EMPTY_MODEL,
+            },
+          },
+        ],
+      }
     );
   }
 }
